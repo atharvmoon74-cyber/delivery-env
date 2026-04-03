c1490968330082eb63cd68a8490511d24ac3ef26
ATHARV NANDKISHOR MOON <atharvmoon74@gmail.com>
2026-04-03 22:19:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from environment import DeliveryEnv
+
+app = FastAPI()
+
+env = None
+
+@app.post("/reset")
+def reset():
+    global env
+    env = DeliveryEnv(difficulty="easy")
+    return env.reset()
+
+@app.post("/step")
+def step(action: int):
+    global env
+    state, reward, done = env.step(action)
+    return {
+        "state": state,
+        "reward": reward,
+        "done": done
+    }
+
+@app.get("/state")
+def state():
+    return env.state()
